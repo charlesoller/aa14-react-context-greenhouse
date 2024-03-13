af14e424ea8ad2d4b2027dbbc56c67df8c733f65
Julia Fout <juuuulash@icloud.com>
2024-03-13 17:30:24 -0500

    finished phase 3

--- src/context/ClimateContext.jsx
+++ src/context/ClimateContext.jsx
@@ -1,2 +1,23 @@
 // Temperature has a default value of 50 degrees
 // Humidity has a default value of 40%
+
+import { createContext, useState, useContext } from "react"
+
+const ClimateContext = createContext();
+
+const useClimate = () => useContext(ClimateContext);
+
+export default function ClimateProvider({ children }) {
+    const [climate, setClimate] = useState(50);
+
+    return (
+      <ClimateContext.Provider
+        value={{
+          climate,
+            setClimate
+        }}
+      >
+        {children}
+      </ClimateContext.Provider>
+    );
+  }
